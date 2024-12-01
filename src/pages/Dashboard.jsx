da9d207ae4f7497d42be1f1c646f246294fb5b0f
delossantos.clementfrancis <clementfrad27@gmail.com>
2024-12-01 18:52:46 +0800

    dashboard styling adjustments

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -15,53 +15,127 @@ const Dashboard = () => {
       <div className="w-[300px] bg-[#1A1F7D] p-4">
         <h2 className="text-white text-lg">Easy Access</h2>
         <div className="flex gap-3 p-2">
-          <div className="flex flex-col justify-center w-fit">
-            <div className="bg-white w-10 h-10 rounded-full border mx-auto"></div>
+          <div className="flex flex-col justify-center w-fit gap-2 text-center items-center">
+            <button className="btn btn-circle">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
             <span className="text-xs">Main Module</span>
           </div>
-          <div className="flex flex-col justify-center w-fit">
-            <div className="bg-white w-10 h-10 rounded-full border mx-auto"></div>
+          <div className="flex flex-col justify-center w-fit gap-2 text-center items-center">
+            <button className="btn btn-circle">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
             <span className="text-xs">Main Module</span>
           </div>
-          <div className="flex flex-col justify-center w-fit">
-            <div className="bg-white w-10 h-10 rounded-full border mx-auto"></div>
+          <div className="flex flex-col justify-center w-fit gap-2 text-center items-center">
+            <button className="btn btn-circle">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
             <span className="text-xs">Main Module</span>
           </div>
-          <div className="flex flex-col justify-center w-fit">
-            <div className="bg-white w-10 h-10 rounded-full border mx-auto"></div>
+          <div className="flex flex-col justify-center w-fit gap-2 text-center items-center">
+            <button className="btn btn-circle">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
             <span className="text-xs">Main Module</span>
           </div>
         </div>
 
         <h2 className="text-white text-lg mt-5">My Tools</h2>
-        <div className="flex flex-col gap-3 p-2">
-          <div className="flex gap-2 justify-center w-fit">
-            <div className="bg-white w-6 h-6 rounded-full border mx-auto"></div>
-            <span className="text-xs my-auto">Main Module</span>
-          </div>
-
-          <div className="flex gap-2 justify-center w-fit ml-8">
-            <div className="bg-white w-6 h-6 rounded-full border mx-auto"></div>
-            <span className="text-xs my-auto">Feature</span>
-          </div>
-
-          <div className="flex gap-2 justify-center w-fit ml-16">
-            <div className="bg-white w-6 h-6 rounded-full border mx-auto"></div>
-            <span className="text-xs my-auto">Feature Action</span>
-          </div>
-        </div>
+        <ul className="menu  rounded-box w-full">
+          <li>
+            <a href="#">Item 1</a>
+          </li>
+          <li>
+            <details>
+              <summary>Parent</summary>
+              <ul>
+                <li>
+                  <a href="#">Submenu 1</a>
+                </li>
+                <li>
+                  <a href="#">Submenu 2</a>
+                </li>
+                <li>
+                  <details>
+                    <summary>Parent</summary>
+                    <ul>
+                      <li>
+                        <a href="#">Submenu 1</a>
+                      </li>
+                      <li>
+                        <a href="#">Submenu 2</a>
+                      </li>
+                    </ul>
+                  </details>
+                </li>
+              </ul>
+            </details>
+          </li>
+          <li>
+            <a href="#">Item 3</a>
+          </li>
+        </ul>
       </div>
 
       {/* Middle Column (75% or 100% when right column is hidden) */}
-      <div
-        className={` bg-[#e1e7ed] p-4 ${
-          !isRightColumnVisible ? "flex-grow" : "flex-grow"
-        }`}
-      >
+      <div className={` bg-[#e1e7ed] p-4 flex-grow flex flex-col`}>
         {/* toggle button */}
         <div
           className={`${
-            isRightColumnVisible ? "hidden" : "flex"
+            isRightColumnVisible ? "hidden" : "flex "
           } justify-end mb-2`}
         >
           <button
@@ -72,15 +146,15 @@ const Dashboard = () => {
           </button>
         </div>
 
-        <div className="bg-[#8186e9] min-h-[50%] p-4 flex flex-col">
-          <p className="text-black mt-auto">GRADING SYSTEM</p>
-          <h2 className="text-black text-3xl font-bold">Grading System</h2>
+        <div className="flex flex-col rounded-3xl overflow-hidden flex-grow">
+          <div className="bg-[#8186e9] min-h-[50%] p-4 flex flex-col justify-end items-start">
+            <p className="text-black mt-auto">GRADING SYSTEM</p>
+            <h2 className="text-black text-3xl font-bold">Grading System</h2>
+          </div>
+          <div className="bg-white min-h-[30%] p-4 flex-grow">
+            <p className="text-black">What does this do?</p>
+          </div>
         </div>
-
-        <div className="bg-white min-h-[30%] p-4">
-          <p className="text-black">What does this do?</p>
-        </div>
-
         <div className="p-4 flex flex-end">
           <button className="w-[100px] font-bold bg-white text-black p-2 rounded ml-auto">
             Start
