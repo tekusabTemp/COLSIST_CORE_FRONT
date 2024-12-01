d4c5728ede09bfa700c0600d343e3193fcc5ac1f
delossantos.clementfrancis <clementfrad27@gmail.com>
2024-12-01 08:20:00 +0800

    additional styling for login page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import coverPage from "./images/coverPage.jpg";
 
 // import { UserProvider } from "./UserContext";
 import NavBar from "./components/Navbar";
@@ -8,18 +9,24 @@ import Login from "./pages/Login";
 
 function App() {
   return (
-
     // <UserProvider value={{ user, setUser, unsetUser }}>
-      <Router>
-        <NavBar />
-        <div className="bg-sky-200 min-h-screen">
-          <Routes>
-            <Route path="/" element={<Register />} />
-            <Route path="/login" element={<Login />} />
-            {/* <Route path="*" element={<Error />} /> */}
-          </Routes>
-        </div>
-      </Router>
+    <Router>
+      <NavBar />
+      <div
+        className="bg-sky-200 min-h-[calc(100vh_-_60px)]"
+        style={{
+          backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${coverPage})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center"
+        }}
+      >
+        <Routes>
+          <Route path="/" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          {/* <Route path="*" element={<Error />} /> */}
+        </Routes>
+      </div>
+    </Router>
     // </UserProvider>
   );
 }
